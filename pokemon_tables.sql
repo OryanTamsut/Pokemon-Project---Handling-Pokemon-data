02a7b415cfd110a5fccf0a80a1fa3736ed5a998c
shaniSolo <sh238296@gmail.com>
2021-07-04 20:27:55 +0300

    get_pokemons_by_trainer

--- pokemon_tables.sql
+++ pokemon_tables.sql
@@ -3,11 +3,15 @@
 CREATE TABLE pokemon(
     id int auto_increment primary key,
     name VARCHAR(20),
-    type VARCHAR(20),
     weight int,
     height int
 );
 
+CREATE TABLE types(
+	id int,
+    type varchar(20),
+    primary key(id,type)
+);
 
 CREATE TABLE trainer(
     name VARCHAR(20) primary key,
--- server.py
+++ server.py
@@ -23,9 +23,22 @@ def update_type():
         if types is not None:
             for type in types:
                 types_array.append(type['type']['name'])
-            queries.update_types(pokemon_name,types_array)
+            queries.update_types(pokemon_name, types_array)
             return Response(json.dumps({"succes": "types added"}), 200)
         else:
             return Response(json.dumps({"err": "not found types"}), 500)
+
+
+@app.route('/get-pokemons', methods=['GET'])
+def get_pokemons_by_trainer():
+    trainer_name = request.args.get("trainer")
+    if trainer_name is None:
+        return Response(json.dumps({"err": "require url parameter: trainer"}), 400)
+    pokemons = queries.find_roster(str(trainer_name))
+    if pokemons is None:
+        return Response(json.dumps({"err": "not found pokemons"}), 500)
+    return Response(json.dumps({"the pokimons are:": pokemons}), 200)
+
+
 if __name__ == '__main__':
     app.run(port=5555)
