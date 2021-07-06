f18d143624bef1a2bdb462862d99fc6dffeb7350
shaniSolo <sh238296@gmail.com>
2021-07-06 10:42:02 +0300

    pokemon project

--- configure.py
+++ configure.py
@@ -1,11 +1,15 @@
 import pymysql
+from flask import Flask, Response, request
 
+app = Flask(__name__)
 
 connection = pymysql.connect(
     host="localhost",
     user="root",
     password="",
-    db="pokemon",
+    db="db_pokemon",
     charset="utf8",
     cursorclass=pymysql.cursors.DictCursor
 )
+
+port = 5555
--- tests.py
+++ tests.py
@@ -4,8 +4,12 @@ url = "http://127.0.0.1:5555"
 
 
 # test 1
-def test_update_types():
-    pass
+def test_get_pokemons_by_types():
+    pokemons = requests.get(url=f'{url}/getPokemonByType?type=normal').json()
+    pokemons = pokemons.get('pokemons')
+    assert "eevee" in pokemons
+    result = requests.put(url=f'{url}/updateType?name=eevee')
+    assert result.status_code == 200
 
 
 # test 2
@@ -32,6 +36,16 @@ def test_add_pokemon():
     assert add_pokemon.status_code == 500 and add_pokemon_mess.get('err') == "pokemon already exist"
 
 
+# test 3
+def test_update_pokemon_types():
+    result = requests.put(url=f'{url}/updateType?name=venusaur')
+    assert result.status_code == 200
+    pokemons = requests.get(url=f'{url}/getPokemonByType?type=poison').json().get('pokemons')
+    assert "venusaur" in pokemons
+    pokemons = requests.get(url=f'{url}/getPokemonByType?type=grass').json().get('pokemons')
+    assert "venusaur" in pokemons
+
+
 # test 4
 def test_get_pokemons_by_owner():
     # get all the pokemon that Dransa have
@@ -47,36 +61,15 @@ def test_get_owners_by_pokemon():
     assert trainers == ["Giovanni", "Jasmine", "Whitney"]
 
 
-# test 1
-def test_get_pokemons_by_types():
-    pokemons = requests.get(url=f'{url}/getPokemonByType?type=normal').json()
-    pokemons = pokemons.get('pokemons')
-    assert "eevee" in pokemons
-    result = requests.put(url=f'{url}/updateType?name=eevee')
-    assert result.status_code == 200
-
-
-# test 3
-def test_update_pokemon_types():
-    result = requests.put(url=f'{url}/updateType?name=venusaur')
-    assert result.status_code == 200
-    pokemons = requests.get(url=f'{url}/getPokemonByType?type=poison').json().get('pokemons')
-    assert "venusaur" in pokemons
-    pokemons = requests.get(url=f'{url}/getPokemonByType?type=grass').json().get('pokemons')
-    assert "venusaur" in pokemons
-
-
-# test 7
+# test 6
 def test_evolve():
-    """
     # try evolve pokemon that can't be evolved
     result = requests.put(url=f'{url}/evolve', json={"pokemon_name": "pinsir", "trainer_name": "Whitney"})
-    assert result.status_code == 500 and result.json().get('error') == "not have a new version"
+    assert result.status_code != 200
 
     # try evolve pokemon that not owned by this trainer
     result = requests.put(url=f'{url}/evolve', json={"pokemon_name": "spearow ", "trainer_name": "Archie"})
-    assert result.status_code == 400 and result.json().get('err') == "this pokemon is not owned by this traniner"
-"""
+    assert result.status_code != 200
 
     # evolve pokemon
     result = requests.put(url=f'{url}/evolve', json={"pokemon_name": "oddish", "trainer_name": "Whitney"})
